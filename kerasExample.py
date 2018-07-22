810e8c5725ab65ff50511a44f54303e42eb2fa41
werowe <werowe@walkerrowe.com>
2018-07-22 13:13:50 +0200

    Update kerasExample.py

--- kerasExample.py
+++ kerasExample.py
@@ -1,17 +1,18 @@
 import keras
 import pandas as pd
 
+
+
 url = 'https://raw.githubusercontent.com/werowe/logisticRegressionBestModel/master/KidCreative.csv'
 
 data = pd.read_csv(url, delimiter=',')
 
-y=data['Buy']
-x = data.iloc[:,2:16]
+labels=data['Buy']
+features = data.iloc[:,2:16]
 
-test_size = 0.33
-seed = 7
-X_train, X_test, Y_train, Y_test = model_selection.train_test_split(x, y, test_size=test_size, random_state=seed)
+model.fit(features, labels, epochs=10, batch_size=32)
+one_hot_labels = keras.utils.to_categorical(labels, num_classes=2)
 
-model = Sequential()
+
 
 
