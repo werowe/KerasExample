e952981630c51aef5aefcc44db2464a638665648
werowe <werowe@walkerrowe.com>
2018-07-26 10:37:57 +0200

    Update kerasExample.py

--- kerasExample.py
+++ kerasExample.py
@@ -1,9 +1,7 @@
-import keras
+import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-
 import pandas as pd
+from keras.layers import Dense
 
 url = 'https://raw.githubusercontent.com/werowe/logisticRegressionBestModel/master/KidCreative.csv'
 
@@ -13,23 +11,23 @@ labels=data['Buy']
 features = data.iloc[:,2:16]
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
+model.add(Dense(units=64, activation='relu', input_dim=1))
+model.add(Dense(units=14, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
               metrics=['accuracy'])
 
-model.summary()
+model.fit(labels, features,
+          batch_size=12,
+          epochs=10,
+          verbose=1,
+          validation_data=(labels, features))
+          
+model.evaluate(labels, features, verbose=0)
 
+model.summary()
 
 
 '''model.compile(optimizer='rmsprop' ,loss='binary_crossentropy',metrics=['accuracy'])
